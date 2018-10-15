ab81747fa649270c4fe5a504870039984d267afc
Onyeka Obi <oobi@BELC02Q6GHWG8WN.sea.corp.expecn.com>
2018-10-15 01:06:48 -0700

    Second commit

--- main.tf
+++ main.tf
@@ -8,7 +8,10 @@ resource "aws_s3_bucket" "crassus" {
 }
 
 resource "aws_s3_bucket_object" "object" {
+  acl = "public-read"
   bucket = "${aws_s3_bucket.crassus.bucket}"
   key = "artifact"
   source = "./hello-server"
+  etag = "${md5(file("./hello-server"))}"
+  content_type = "application/octet-stream"
 }
